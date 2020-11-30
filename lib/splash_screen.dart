8385851eb3158afd2c6065dbaf9510c77e842c16
Laxmi Nagaral <60965046+laxminagaral@users.noreply.github.com>
2020-12-01 00:10:14 +0530

    Update splash_screen.dart

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:shop_app/loginpage.dart';
 import 'package:shop_app/screens/home/home_screen.dart';
@@ -14,33 +15,36 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    Timer(Duration(seconds: 5), ()=>Navigator.push(context, MaterialPageRoute(builder: (context)=>LoginPage())));
+    User user = FirebaseAuth.instance.currentUser;
+    print(user);
+    Timer(
+        Duration(seconds: 5),
+        () => {
+              if (user == null)
+                {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => LoginPage()))
+                }
+              else
+                {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => HomeScreen()))
+                }
+            });
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-              child: Column(
+        child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment:CrossAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
-            Image.asset("assets/images/splashlogo.jpeg",
-              height:150), 
-          
+            Image.asset("assets/images/splashlogo.jpeg", height: 150),
           ],
-
         ),
       ),
-
-    
-      
-    
-      
     );
   }
 }
-
-
-
-
-
